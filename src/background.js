a3cd69493c6be493df9a860b5afe6d510e33f6e9
Janko <janko.huic@gmail.com>
2025-01-26 13:35:13 +0100

    allows cycling through <input> elements and smart saves press count in url table

--- src/background.js
+++ src/background.js
@@ -4,11 +4,47 @@ browser.commands.onCommand.addListener(async (command) => {
   if (command === "focus-searchbox") {
     console.log("keybind pressed");
 
-    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-      browser.tabs.sendMessage(tabs[0].id, {
-        method: "focusInput",
-        urlTable: storage.urlTable,
+    browser.storage.sync.get("urlTable").then((storage) => {
+      browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, {
+          method: "focusInput",
+          urlTable: storage.urlTable,
+        });
       });
     });
   }
 });
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request);
+  if (request.method === "updateUrlTable") {
+    console.log(request);
+    updateUrlTable(request);
+  } else if (request.method === "getUrlTable") {
+    console.log("fetching url table");
+    sendUrlTable(sendResponse);
+    return true;
+  }
+});
+
+function sendUrlTable(sendResponse) {
+  browser.storage.sync
+    .get("urlTable")
+    .then((storage) => sendResponse({ urlTable: storage.urlTable || {} }));
+}
+
+function updateUrlTable(request) {
+  if (Number.isInteger(request.pressCount)) {
+    browser.storage.sync.get("urlTable").then((storage) => {
+      let newUrlTable = storage["urlTable"] || {};
+      newUrlTable[request.urlBase] = request.pressCount;
+      browser.storage.sync.set({ urlTable: newUrlTable });
+      console.log(
+        "urlTable updated for, ",
+        request.urlBase,
+        ": ",
+        request.pressCount
+      );
+    });
+  }
+}
--- src/focus.js
+++ src/focus.js
@@ -1,9 +1,82 @@
-let inputBox = document.querySelectorAll("input");
+let lastPress = Date.now() - 3000;
+let pressCount;
 
-browser.runtime.onMessage.addListener((request) => {
-  if (request.method === "focusInput") {
-    if (inputBox) {
-      inputBox[pressCount].focus();
+browser.runtime.sendMessage({ method: "getUrlTable" }).then((response) => {
+  let urlTable = response.urlTable;
+  console.log(urlTable);
+
+  browser.runtime.onMessage.addListener((request) => {
+    if (request.method === "focusInput") {
+      let inputBoxArray = document.querySelectorAll("input");
+
+      if (inputBoxArray) {
+        let nFields = inputBoxArray.length;
+        let urlBase = getUrlBase();
+        let sinceLastPress = Date.now() - lastPress;
+        let isInUrlTable = Object.keys(urlTable).includes(urlBase);
+
+        if (sinceLastPress > 3000) {
+          if (isInUrlTable) {
+            pressCount = urlTable[urlBase];
+            console.log("urlbase in storage, pressCount: ", String(pressCount));
+            focusElement(inputBoxArray[pressCount % nFields]);
+          } else {
+            pressCount = 0;
+            focusElement(inputBoxArray[pressCount % nFields]);
+          }
+        } else {
+          pressCount++;
+          focusElement(inputBoxArray[pressCount % nFields]);
+          storePressCount(pressCount % nFields);
+          console.log("stored presscount of ", String(pressCount % nFields));
+        }
+
+        console.log(pressCount);
+        lastPress = Date.now();
+      }
+    }
+  });
+});
+
+function storePressCount(pressCount) {
+  let urlBase = getUrlBase();
+
+  browser.runtime.sendMessage({
+    method: "updateUrlTable",
+    urlBase,
+    pressCount,
+  });
+}
+
+function getUrlBase() {
+  let currentUrl = window.location.href;
+  let urlSplit = currentUrl.split(".");
+  let urlBase;
+
+  if (urlSplit.length === 1) return "";
+
+  if (currentUrl.includes("www")) {
+    urlBase = urlSplit[1];
+  } else {
+    let remainder = urlSplit[0];
+    if (remainder.includes("//")) {
+      urlBase = remainder.split("//").slice(-1)[0];
+    } else {
+      urlBase = urlSplit[0];
     }
   }
-});
+
+  return urlBase;
+}
+
+function focusElement(element) {
+  try {
+    if (element && typeof element.focus === "function") {
+      element.focus();
+    } else {
+      console.log("Element is not focusable:", element);
+    }
+  } catch (error) {
+    console.error("error attempting to focus input element");
+  }
+}
