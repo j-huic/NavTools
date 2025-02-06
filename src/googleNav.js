45c8cd920481819c2571d8fbaa07cc01d4a0b2da
Janko <janko.huic@gmail.com>
2025-02-06 20:31:15 +0100

    simple working fix to ignore child node hrefs

--- src/googleNav.js
+++ src/googleNav.js
@@ -1,8 +1,8 @@
 browser.runtime.onMessage.addListener((request) => {
   console.log("msg received");
   if (request.method === "clickCard") {
-    let cards = Array.from(document.querySelectorAll(".MjjYud a"));
-    href = cards[request.index]?.href;
+    let cards = Array.from(document.querySelectorAll(".MjjYud"));
+    let href = cards[request.index].querySelector("a").href;
 
     browser.runtime.sendMessage({ method: "openTab", url: href });
   }
